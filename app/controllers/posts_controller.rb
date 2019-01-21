37feb51c2db75913700235dc962c776cc083b40e
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 19:37:33 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,17 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+	def edit
+		 @post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		# raise @post.inspect
+		@post.update(title: params[:post][:title], description: params[:post][:description])
+		# raise @post.inspect
+		redirect_to post_path(@post)
+	end
+
 end
